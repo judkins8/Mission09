9b8c70266f6010896a7d67628aa5e7b5798009c9
judkins8 <joshua.judkins@gmail.com>
2023-03-01 23:54:13 -0700

    commit

---
+++ Mission09/Models/EFBookstoreRepository.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mission09.Models;
+
+namespace Mission09.Models
+{
+    public class EFBookstoreRepository : IBookstoreRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFBookstoreRepository(BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Books> Books => context.Books;
+    }
+}
