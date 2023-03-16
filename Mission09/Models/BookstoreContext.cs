626f576ca2ef65ee49250479e26f4f3977e67de0
judkins8 <joshua.judkins@gmail.com>
2023-03-16 03:37:30 -0600

    Mission11

--- Mission09/Models/BookstoreContext.cs
+++ Mission09/Models/BookstoreContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -21,6 +18,7 @@ namespace Mission09.Models
         }
 
         public DbSet<Books> Books { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
 
 
     }
--- Mission09/Models/Cart.cs
+++ Mission09/Models/Cart.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using Mission09.Models;
 
 namespace Mission09.Models
 {
@@ -10,7 +10,7 @@ namespace Mission09.Models
     {
         public List<CartLineItem> Books { get; set; } = new List<CartLineItem>();
 
-        public void AddBook(Books book, int qty)
+        public virtual void AddBook(Books book, int qty)
         {
             CartLineItem line = Books
                 .Where(b => b.Book.BookId == book.BookId)
@@ -29,6 +29,16 @@ namespace Mission09.Models
                 line.Quantity += qty;
             }
         }
+
+        public virtual void RemoveBook(Books book)
+        {
+            Books.RemoveAll(x => x.Book.BookId == book.BookId);
+        }
+
+        public virtual void ClearCart()
+        {
+            Books.Clear();
+        }
         public double CalculateTotal()
         {
             double sum = Books.Sum(x => x.Quantity * x.Book.Price);
@@ -41,6 +51,7 @@ namespace Mission09.Models
 
     public class CartLineItem
     {
+        [Key]
         public int LineID { get; set; }
         public Books Book { get; set; }
         public int Quantity { get; set; }
